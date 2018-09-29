b7a8f95bf8a55b3420bc8ea26b81e354f2272cf9
goatez <whfrederick3@gmail.com>
2018-09-29 00:41:02 -0400

    Commit to branch

--- strings_framework2_template.py
+++ strings_framework2_template.py
@@ -9,12 +9,37 @@ with open("data/20170405twitterdirty.txt", "r") as fin, open("data/cleaned_twitt
 
         if line.startswith("#"):  # skip it if the line begins with a #
             continue
+        # if line starts with blank line - how to search for blank line character
+
+        if line == '\n': # deletes blank line with no white space character
+            continue
+        if line == 'NA': # deletes 'NA' entry
+            continue
+        if "," in line: # deletes entry with "," - line 667,701
+            #print(line)
+            continue
 
         line = line.lower()     # convert to lower case
         line = line.strip()     # remove leading/trailing blanks
 
-        print(line)
+        # if line.startwith("@")
+        # if line is just twitter handle
+        # line 657 - has period
 
-        Dirty_Twitter_fields_list.append(line)  # add the cleaned item to the list
+        if line.startswith("https://twitter.com/"):
+            #print(line)
+            Dirty_Twitter_fields_list.append(line+"\n")  # add the cleaned item to the list
 
-print(Dirty_Twitter_fields_list)
+        if line.startswith("http://www.twitter.com/"):
+            #print(line)
+            #print(line.replace("http://www.", "https://"))
+            Dirty_Twitter_fields_list.append(line.replace("http://www.", "https://")+"\n") # add the cleaned item to the list
+
+        if line.startswith("@"):
+            print(line)
+            print(line.replace("@", "https://twitter.com/"))
+            Dirty_Twitter_fields_list.append(line.replace("@", "https://twitter.com/")+"\n")  # add the cleaned item to the list
+
+    #print(Dirty_Twitter_fields_list)
+    fou.writelines(Dirty_Twitter_fields_list)
+    fou.close()
